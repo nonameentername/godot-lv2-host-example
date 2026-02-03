8a6d8360a78767e7f621befce66a0757cb76b391
werner mendizabal <nonameentername@gmail.com>
2026-02-03 13:56:08 -0600

    Update project to use godot v4.6

--- package.gd
+++ package.gd
@@ -2,4 +2,4 @@ extends "res://addons/godot-package/package.gd"
 
 
 func _requirements():
-	dependency("nonameentername/godot-lv2-host", {"tag": "v0.1.0-beta.1"})
+	dependency("nonameentername/godot-lv2-host", {"tag": "v0.1.0-beta.5"})
